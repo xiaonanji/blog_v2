9ec1e35430ea8dfe06f4361e00f4c2d912c3b958
xiaonanji <sticker.ji@gmail.com>
2025-12-05 20:48:57 +1100

    Fix error

--- .eleventy.js
+++ .eleventy.js
@@ -77,9 +77,7 @@ module.exports = function (eleventyConfig) {
     "youtube", (videoURL, title) => {
       const url = new URL(videoURL);
       const id = url.searchParams.get("v");
-      return `<iframe class="yt-shortcode" src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video player${
-        title ? ` for ${title}` : ""
-      }" frameborder="0" allowfullscreen></iframe>`;
+      return `<iframe class="yt-shortcode" src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video player${title ? ` for ${title}` : ""}" frameborder="0" allowfullscreen></iframe>`;
     }
   );
 
